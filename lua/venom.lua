75d25cb2b06c84847598955336c2c49dbe21b0dc
Rafael Bodill <justrafi@gmail.com>
2023-03-31 17:28:14 +0300

    Refactor and add tool support

--- lua/venom.lua
+++ lua/venom.lua
@@ -1,137 +1,215 @@
 -- venom: discover venv and automatically set LSP servers
--- https://github.com/rafi/vim-config
+-- https://github.com/rafi/neoconf-venom.nvim
 
-local Path = require('lspconfig.util').path
+local is_windows = vim.loop.os_uname().version:match('Windows')
 local Util = require('neoconf.util')
+local has_plenary, Job = pcall(require, 'plenary.job')
+if not has_plenary then
+	Util.error('venom requires https://github.com/nvim-lua/plenary.nvim')
+	return
+end
+local Path = require('plenary.path')
 
-local opts = {}
-
+---@class VenomConfig
 local default_opts = {
-	auto_activate = true,
 	echo = true,
-	quiet = false,
 	symbol = '🐍',
-	root_patterns = { 'venv', '.venv', '.python-version' },
+	venv_patterns = { 'venv', '.venv', '.python-version' },
 	use_tools = true,
 	tools = {
-		poetry = 'poetry env info -p',
-		pipenv = 'pipenv --venv',
+		pipenv = { 'pipenv', '--venv' },
+		poetry = { 'poetry', 'env', 'info', '-p' },
+	},
+	---@type table<string, fun(venv_path:string):table>
+	plugins = {
+		--- pyright
+		---@param venv_path string
+		---@return table
+		pyright = function(venv_path)
+			return {
+				python = {
+					pythonPath = table.concat({ venv_path, 'bin', 'python' }, '/')
+				},
+			}
+		end,
+
+		--- pylsp
+		---@param venv_path string
+		---@return table
+		pylsp = function(venv_path)
+			return {
+				pylsp = {
+					plugins = { jedi = { environment = venv_path } }
+				},
+			}
+		end,
 	},
 }
 
--- Will contain root directories as keys, and found venv python bin path
--- as the value. For example: { '/home/foo/bar' = '/home/foo/bar/venv' }
+---@type VenomConfig
+local opts = {}
+
+---@type table<string, string>
 local cached_venvs = {}
+
+---@type table<string, boolean>
 local cached_unresolved_paths = {}
 
--- Supported LSP plugins setters.
-local lsp_plugins = {
-	pyright = function(venv_path)
-		local venv_python_path = Path.join(venv_path, 'bin', 'python')
+---@param filename string
+---@return boolean
+local function is_dir(filename)
+	return Util.exists(filename) == 'directory'
+end
 
-		return {
-			python = {
-				pythonPath = venv_python_path
-			},
-		}
-	end,
+---@param filename string
+local function is_absolute(filename)
+	if is_windows then
+		return filename:match('^%a:') or filename:match('^\\\\')
+	else
+		return filename:match('^/')
+	end
+end
 
-	pylsp = function(venv_path)
-		return {
-			pylsp = {
-				plugins = { jedi = { environment = venv_path } }
-			},
-		}
-	end,
-}
+---@param ... string
+---@return string
+local function path_join(...)
+	return table.concat(vim.tbl_flatten { ... }, '/')
+end
 
--- Find files/directories in a specific path.
----@param path string
----@param patterns table
-local function find_pattern(path, patterns)
-	for _, pattern in ipairs(patterns) do
-		local path_joined = Path.join(path, pattern)
-		for _, p in ipairs(vim.fn.glob(path_joined, true, true)) do
-			if Path.exists(p) then
-				return p
+--- Verifies python executable exists in a venv.
+---@param venv_path string
+---@return boolean
+local function is_venv(venv_path)
+	if venv_path == '' or venv_path == nil then
+		return false
+	end
+	local bin = path_join(venv_path, 'bin', 'python')
+	return is_dir(venv_path) and Util.exists(bin)
+end
+
+-- Use predefined executables to find virtualenv's location.
+---@return string
+local function find_with_tools()
+	for _, cmd in pairs(opts.tools) do
+		if type(cmd) ~= 'table' then
+			Util.error('[venom] tools value has to be a table')
+			return ''
+		end
+
+		local command = table.remove(cmd, 1)
+		local stderr = {}
+
+		if vim.fn.executable(command) == 1 then
+			local stdout, ret = Job:new({
+				command = command,
+				args = cmd,
+				on_stderr = function(_, data)
+					table.insert(stderr, data)
+				end,
+			}):sync()
+
+			if ret == 0 then
+				if is_venv(stdout[1]) then
+					return stdout[1]
+				end
+			end
+			if #stderr > 0 then
+				Util.error(
+					string.format('Erroneous shell output from %s: %s',
+						command, vim.inspect(stderr))
+				)
 			end
 		end
 	end
+	return ''
 end
 
 -- Finds a virtual-environment by using multiple strategies for provided
 -- project path:
--- 1. Searches for {venv, .venv, .python-version} and:
+-- Searches for {venv, .venv, .python-version} and:
 --   a. If directory, use as virtual-environment. Great for in-project venv/
 --   b. If a file, read the first line as path. Good for pyenv/virtualenvwrapper
--- 2. Use external tools:
---   a. poetry
---   b. pipenv
 ---@param path string
+---@return string
 local function find_virtualenv(path)
-	local pyenv_root = vim.env['PYENV_ROOT']
-	local workon_home = vim.env['WORKON_HOME']
+	local venv_locations = {}
+	for _, dir in ipairs({
+		path_join(vim.env['PYENV_ROOT'], 'versions'),
+		vim.env['WORKON_HOME']
+	}) do
+		if Util.exists(dir) then
+			table.insert(venv_locations, dir)
+		end
+	end
 
 	-- Try to find certain directory names or placeholder text files that
 	-- are probably the virtual-environment we're looking for.
-	local found_path = find_pattern(path, opts.root_patterns)
-	if not (found_path == nil or found_path == '') then
-		if Path.is_dir(found_path) then
+	local found_paths = vim.fs.find(opts.venv_patterns, {
+		upward = true,
+		stop = vim.loop.os_homedir(),
+		path = path,
+		limit = 3,
+	})
+
+	-- Return first found match.
+	for _, found_path in ipairs(found_paths) do
+		if found_path == nil or found_path == '' then
+			-- skip
+		elseif is_venv(found_path) then
 			return found_path
-		elseif Path.is_file(found_path) then
+		elseif Util.exists(found_path) == 'file' then
 			-- Read location of virtual-environment from text-file
-			local file = io.open(found_path)
-			local user_dir = ''
-			if file ~= nil then
-				user_dir = file:read('*l')
-				file:close()
-			end
+			local user_dir = Path:new(found_path):head(1)
 			if not (user_dir == nil or user_dir == '') then
 				-- Use file contents as an absolute path
-				if Path.is_absolute(user_dir) and Path.is_dir(user_dir) then
+				if is_absolute(user_dir) and is_venv(found_path) then
 					return user_dir
 				end
-				-- Use file contents as a pyenv version
-				local pyenv_version = Path.join(pyenv_root, 'versions', user_dir)
-				if pyenv_version ~= '' and Path.is_dir(pyenv_version) then
-					return pyenv_version
-				end
-				-- Use file contents as a virtualenvwrapper directory
-				local workon_dir = Path.join(workon_home, user_dir)
-				if workon_dir ~= '' and Path.is_dir(workon_dir) then
-					return workon_dir
+				-- Use file contents as a pyenv/virtualenvwrapper version
+				for _, venv_location in ipairs(venv_locations) do
+					venv_location = path_join(venv_location, user_dir)
+					if is_venv(venv_location) then
+						return venv_location
+					end
 				end
 			end
 		end
 	end
-
-	-- Use predefined executables to find virtualenv's location.
-	-- TODO:
 	return ''
 end
 
--- Update LSP configurations for various plugins.
+---@param venv_path string
+---@param setter fun(venv_path:string):table
+---@param client lsp.Client
+---@return boolean
+local function apply_function(venv_path, setter, client)
+	local new_config = setter(venv_path)
+	if not vim.tbl_isempty(new_config) then
+		Util.merge(client.config.settings, new_config)
+		local ok = pcall(client.notify, 'workspace/didChangeConfiguration', {
+			settings = nil,
+			-- settings = new_config,
+		})
+		return ok
+	end
+	return false
+end
+
+-- Update LSP configurations for various Venom plugins.
 ---@param client lsp.Client
 ---@param venv_path string
-local function update_client_python_path(client, venv_path)
-	for lsp_name, setter in pairs(lsp_plugins) do
+local function apply_venom_plugins(client, venv_path)
+	for lsp_name, setter in pairs(opts.plugins) do
 		if client.name == lsp_name then
-			local new_config = setter(venv_path)
-			Util.merge(client.config.settings, new_config)
-			local ok = pcall(client.notify, 'workspace/didChangeConfiguration', {
-				settings = new_config,
-			})
 			local msg = vim.fn.fnamemodify(venv_path, ':~')
-			if ok then
+			if apply_function(venv_path, setter, client) then
 				if opts.echo then
-					vim.notify(msg, vim.log.levels.INFO, {
-						title = 'Virtual-environment set (' .. lsp_name .. ')'
-					})
+					local title = 'Virtual-environment set (' .. lsp_name .. ')'
+					vim.notify(msg, vim.log.levels.INFO, { title = title })
 				end
 			else
-				vim.notify(msg, vim.log.levels.ERROR, {
-					title = 'Failed setting virtual-environment (' .. lsp_name .. ')'
-				})
+				local title = 'Failed setting virtual-environment (' .. lsp_name .. ')'
+				vim.notify(msg, vim.log.levels.ERROR, { title = title })
 			end
 		end
 	end
@@ -139,11 +217,11 @@ end
 
 -- On init hook for LSP clients. Automatically sets found virtualenv path.
 ---@param root_dir string
-local function on_init(root_dir)
+local function lsp_client_on_init(root_dir)
 	return function(client)
 		-- First look in cached paths.
 		if cached_venvs[root_dir] ~= nil then
-			update_client_python_path(client, cached_venvs[root_dir])
+			apply_venom_plugins(client, cached_venvs[root_dir])
 			return true
 		end
 		if cached_unresolved_paths[root_dir] == true then
@@ -152,71 +230,70 @@ local function on_init(root_dir)
 
 		-- Find virtualenv's python binary with multiple methods.
 		local venv_path = find_virtualenv(root_dir)
-		if venv_path == '' or not Path.is_dir(venv_path) then
-			cached_unresolved_paths[root_dir] = true
-			return
+		if venv_path == '' or not is_dir(venv_path) then
+			if opts.use_tools then
+				-- Use predefined executables to find virtualenv's location.
+				venv_path = find_with_tools()
+			end
+			if venv_path == '' or not is_dir(venv_path) then
+				cached_unresolved_paths[root_dir] = true
+				return
+			end
 		end
 
 		-- Cache and update LSP clients with found venv python binary.
 		cached_venvs[root_dir] = venv_path
 		vim.api.nvim_buf_set_var(0, 'virtual_env', venv_path)
-		update_client_python_path(client, venv_path)
+		apply_venom_plugins(client, venv_path)
 		return true
 	end
 end
 
----@param path string
-local function basename(path)
-	return string.gsub(path, '(.*/)(.*)', '%2')
-end
-
-local function statusline()
-	local venv_path = vim.b['virtual_env'] or os.getenv('VIRTUAL_ENV')
-
-	if not (venv_path == nil or venv_path == '') then
-		return basename(venv_path) .. ' ' .. opts.symbol
-	end
-	return ''
-end
-
 -- Setup LSP on_init hooks as Neoconf plugin.
 ---@param plugin_name string
-local function setup_plugin(plugin_name)
+local function setup_neoconf_plugin(plugin_name)
 	return function()
 		Util.on_config({
 			name = 'settings/plugins/' .. plugin_name,
 			on_config = function(client, root_dir)
 				if client.name == plugin_name then
-					client.on_init = on_init(root_dir)
+					client.on_init = lsp_client_on_init(root_dir)
 				end
 			end,
 		})
 	end
 end
 
--- Setup Venom
+local M = {}
+
+--- Statusline friendly Venom section.
+---@return string
+function M.statusline()
+	local venv_path = vim.b['virtual_env'] or vim.env.VIRTUAL_ENV
+	if not (venv_path == nil or venv_path == '') then
+		return vim.fs.basename(venv_path) .. ' ' .. opts.symbol
+	end
+	return ''
+end
+
+-- Setup Venom: Register as neoconf plugins.
 ---@param user_opts? table
-local function setup(user_opts)
+function M.setup(user_opts)
 	opts = Util.merge({}, default_opts, user_opts or {})
 	vim.validate({
-		auto_activate = { opts.auto_activate, 'b', true },
 		echo = { opts.echo, 'b', true },
-		quiet = { opts.quiet, 'b', true },
 		symbol = { opts.symbol, 's', true },
-		root_patterns = { opts.root_patterns, 't', true },
+		venv_patterns = { opts.venv_patterns, 't', true },
 		use_tools = { opts.use_tools, 'b', true },
 		tools = { opts.tools, 't', true },
 	})
 
 	-- Register Neoconf plugins.
-	for lsp_name, _ in pairs(lsp_plugins) do
+	for lsp_name, _ in pairs(opts.plugins) do
 		require('neoconf.plugins').register({
-			setup = setup_plugin(lsp_name),
+			setup = setup_neoconf_plugin(lsp_name),
 		})
 	end
 end
 
-return {
-	setup = setup,
-	statusline = statusline,
-}
+return M
