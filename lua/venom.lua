1540c310e4f82425a2dae62241e87d61c9e31856
mkdir700 <mkdir700@gmail.com>
2023-06-02 22:50:04 +0800

    refactor: set variables to buffers in subdirectories of same root directory.

--- lua/venom.lua
+++ lua/venom.lua
@@ -269,7 +269,13 @@ local function lsp_client_on_init(root_dir)
 
 		-- Cache and update LSP clients with found venv python binary.
 		cached_venvs[root_dir] = venv_path
-		vim.api.nvim_buf_set_var(0, 'virtual_env', venv_path)
+		-- just path is child path of root_dir
+		for _, buffer in ipairs(vim.api.nvim_list_bufs()) do
+			local path = vim.api.nvim_buf_get_name(buffer)
+			if is_subdirectory(path, root_dir) then
+				vim.api.nvim_buf_set_var(buffer, "virtual_env", venv_path)
+			end
+		end
 		apply_venom_plugins(client, venv_path)
 		return true
 	end
